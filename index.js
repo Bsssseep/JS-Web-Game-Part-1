3c4bdb3bd12e35eea7c9813af795a233d8675994
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2023-05-30 16:06:04 -0700

    updated Game

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+//this function allows my images to all have the 
+
+
+
+function newImage (url,left,bottom) {
+    let image = document.createElement('img')
+    image.src = url
+    image.style.position = 'fixed';
+    image.style.left = left + 'px';
+    image.style.bottom = bottom +'px';
+    document.body.append(image)
+    return image
+} 
+
+// images!
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+
+
+
+
+// this is the click function 
+
+function newItem(url,left,bottom) {
+
+
+let image = document.createElement('img')
+image.src = url;
+image.style.position = 'fixed';
+image.style.left = left + 'px';
+image.style.bottom = bottom + 'px';
+document.body.append(image)
+
+image.addEventListener('dblclick', function(){
+    image.remove()
+});
+
+
+
+   
+// New Items     
+   
+
+}
+
+
+
+newItem('assets/sword.png', 500, 405);
+newItem('assets/shield.png', 165, 185);
+newItem('assets/staff.png', 600, 100);
+
+
+//backdrop sky and grass
+
+
+
